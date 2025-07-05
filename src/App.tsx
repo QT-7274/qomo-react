3561fe8693c928594f05ce97a153075e2b4db685
QT-7274 <2531376773@qq.com>
2025-07-05 15:16:54 +0800

    feat: 测试双仓库推送情况

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Qomo</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          qomo is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
